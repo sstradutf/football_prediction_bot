b257a97fb3dc5dd70b9b3b9e94d31b74ccb6c7e2
Ghandy MEBALE <145226666+sstradutf@users.noreply.github.com>
2025-05-13 22:04:08 +0200

    Update telegram_alert.py

--- football_prediction_bot/telegram_alert.py
+++ football_prediction_bot/telegram_alert.py
@@ -1,17 +1,7 @@
 import requests
 from .config import TELEGRAM_TOKEN, CHAT_ID
 
-# Ajoute ton propre ID ici
-AUTHORIZED_USER_ID = 6567626016
-
-def send_message(text: str, chat_id: int = AUTHORIZED_USER_ID):
-    token = TELEGRAM_TOKEN  # importé depuis config.py
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": text,
-        "parse_mode": "HTML"
-    }
-
-    response = requests.post(url, json=payload)
-    return response.json()
+def send_message(message):
+    url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
+    data = {'chat_id': CHAT_ID, 'text': message}
+    requests.post(url, data=data)
